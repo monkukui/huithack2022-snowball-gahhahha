b5a87b16e9f5b31623e399d9f14572b106dd759f
negitorogithub <s_tsuruta@toridori.co.jp>
2022-01-15 19:49:03 +0900

    save

---
+++ frontend/src/game.js
@@ -0,0 +1,280 @@
+import * as THREE from "three";
+
+export const Game = (enemyName) => {
+  socket.on("fall", (jsonString) => {
+    const data = JSON.parse(jsonString).data;
+    const x = data.x;
+    const y = data.y;
+    syncGenerateSnowball(x, y);
+  });
+
+  socket.on("enemyPosition", (jsonString) => {
+    const data = JSON.parse(jsonString).data;
+    const x = data.x;
+    const y = data.y;
+    syncAnotherPlayer(x, y);
+  });
+
+  socket.on("disconnected", () => {});
+  socket.on("over", () => {});
+
+  const snowBallInitialZ = 150;
+
+  const materialBlue = new THREE.MeshBasicMaterial({
+    color: 0x0000ff,
+  });
+  const materialSky = new THREE.MeshBasicMaterial({
+    color: 0x005555,
+  });
+
+  const materialFloor = new THREE.MeshBasicMaterial({
+    color: 0xc05050,
+  });
+
+  const materialShadow = new THREE.MeshBasicMaterial({
+    color: 0x202020,
+  });
+
+  const playerRadius = 10;
+  const playerHeight = 20;
+  const ballRadius = 14;
+  const generateSnowBallTicks = 35;
+  const floorScale = 200;
+  const snowballSpeedFactor = 1.5;
+  const keyInput = {
+    up: false,
+    down: false,
+    left: false,
+    right: false,
+  };
+
+  const speedFactor = 2;
+
+  const makeFloor = () => {
+    const geometry = new THREE.PlaneGeometry(floorScale, floorScale);
+
+    const mesh = new THREE.Mesh(geometry, materialFloor);
+    mesh.name = "floor";
+    scene.add(mesh);
+  };
+  makeFloor();
+
+  const makePlayer = () => {
+    const geometry = new THREE.CylinderGeometry(
+      playerRadius,
+      playerRadius,
+      playerHeight,
+      32
+    );
+
+    const cylinder = new THREE.Mesh(geometry, materialBlue);
+    cylinder.name = "cylinder1";
+    cylinder.position.x = -50;
+    cylinder.rotation.x = Math.PI / 2;
+    cylinder.position.z = playerHeight / 2;
+    scene.add(cylinder);
+  };
+  makePlayer();
+
+  const makeAnotherPlayer = () => {
+    const geometry = new THREE.CylinderGeometry(
+      playerRadius,
+      playerRadius,
+      playerHeight,
+      32
+    );
+    const cylinder = new THREE.Mesh(geometry, materialSky);
+    cylinder.name = "cylinder2";
+    cylinder.position.x = 50;
+    cylinder.rotation.x = Math.PI / 2;
+    cylinder.position.z = playerHeight / 2;
+
+    scene.add(cylinder);
+  };
+  makeAnotherPlayer();
+
+  const setupKeyInput = () => {
+    document.onkeydown = function (e) {
+      switch (e.key) {
+        case "ArrowLeft":
+          keyInput.left = true;
+          break;
+        case "ArrowRight":
+          keyInput.right = true;
+          break;
+        case "ArrowDown":
+          keyInput.down = true;
+          break;
+        case "ArrowUp":
+          keyInput.up = true;
+          break;
+      }
+    };
+    document.onkeyup = function (e) {
+      switch (e.key) {
+        case "ArrowLeft":
+          keyInput.left = false;
+          break;
+        case "ArrowRight":
+          keyInput.right = false;
+          break;
+        case "ArrowDown":
+          keyInput.down = false;
+          break;
+        case "ArrowUp":
+          keyInput.up = false;
+          break;
+      }
+    };
+  };
+  setupKeyInput();
+
+  const tickMoveByKey = () => {
+    var cylinder1 = scene.getObjectByName("cylinder1");
+    const vector2 = {
+      x: 0,
+      y: 0,
+      normalize: () => {
+        const length = Math.sqrt(vector2.x * vector2.x + vector2.y * vector2.y);
+        if (length === 0) {
+          return;
+        }
+        if (vector2.x !== 0) {
+          vector2.x = vector2.x / length;
+        }
+        if (vector2.y !== 0) {
+          vector2.y = vector2.y / length;
+        }
+      },
+    };
+
+    if (keyInput.left) {
+      vector2.x -= 1;
+    }
+    if (keyInput.right) {
+      vector2.x += 1;
+    }
+    if (keyInput.down) {
+      vector2.y -= 1;
+    }
+    if (keyInput.up) {
+      vector2.y += 1;
+    }
+    vector2.normalize();
+    cylinder1.position.x += vector2.x * speedFactor;
+    cylinder1.position.y += vector2.y * speedFactor;
+  };
+
+  const tickGenerateSnowBalls = () => {
+    var geometry = new THREE.SphereGeometry(ballRadius, 32, 32);
+    var material = new THREE.MeshBasicMaterial({ color: 0xffffff });
+    var sphere = new THREE.Mesh(geometry, material);
+    const boundBoxVertical = floorScale - ballRadius * 2;
+    sphere.position.x = Math.random() * boundBoxVertical - boundBoxVertical / 2;
+    sphere.position.y = Math.random() * boundBoxVertical - boundBoxVertical / 2;
+    sphere.position.z = snowBallInitialZ;
+    scene.add(sphere);
+    sphere.name = "snowball";
+  };
+
+  const tickSnowballsAndShadow = () => {
+    const deleteShadow = () => {
+      var shadows = scene.children.filter(
+        (child) => child.name === "snowballShadow"
+      );
+      shadows.forEach((shadow) => {
+        scene.remove(shadow);
+      });
+    };
+    deleteShadow();
+
+    var balls = scene.children.filter((child) => child.name === "snowball");
+    balls.forEach((ball) => {
+      const move = () => {
+        ball.position.z -=
+          ((snowBallInitialZ - ball.position.z + 1) / 20) * snowballSpeedFactor;
+        if (ball.position.z < 0) {
+          scene.remove(ball);
+        }
+      };
+      move();
+
+      const makeShadow = () => {
+        const geometry = new THREE.CircleGeometry(ballRadius, 32);
+        const material = materialShadow;
+        const shadow = new THREE.Mesh(geometry, material);
+        shadow.name = "snowballShadow";
+        shadow.position.x = ball.position.x;
+        shadow.position.y = ball.position.y;
+        shadow.position.z = 0.2;
+        scene.add(shadow);
+      };
+      makeShadow();
+    });
+  };
+
+  const tickWallBlock = () => {
+    var player = scene.getObjectByName("cylinder1");
+    if (player.position.x > floorScale / 2 - playerRadius) {
+      player.position.x = floorScale / 2 - playerRadius;
+    }
+    if (player.position.x < -floorScale / 2 + playerRadius) {
+      player.position.x = -floorScale / 2 + playerRadius;
+    }
+    if (player.position.y > floorScale / 2 - playerRadius) {
+      player.position.y = floorScale / 2 - playerRadius;
+    }
+    if (player.position.y < -floorScale / 2 + playerRadius) {
+      player.position.y = -floorScale / 2 + playerRadius;
+    }
+  };
+
+  const tickSnowBallCollision = () => {
+    var player = scene.getObjectByName("cylinder1");
+    var balls = scene.children.filter((child) => child.name === "snowball");
+    balls.forEach((ball) => {
+      if (
+        ball.position.distanceTo(player.position) <
+        playerRadius + ballRadius
+      ) {
+        console.log("hit");
+      }
+    });
+  };
+
+  const syncAnotherPlayer = (x, y) => {
+    var cylinder2 = scene.getObjectByName("cylinder2");
+    cylinder2.position.x = x;
+    cylinder2.position.y = y;
+  };
+
+  const syncGenerateSnowball = (x, y) => {
+    var geometry = new THREE.SphereGeometry(ballRadius, 32, 32);
+    var material = new THREE.MeshBasicMaterial({ color: 0xffffff });
+    var sphere = new THREE.Mesh(geometry, material);
+    sphere.position.x = x;
+    sphere.position.y = y;
+    sphere.position.z = snowBallInitialZ;
+    scene.add(sphere);
+    sphere.name = "snowball";
+  };
+
+  renderer.setSize(window.innerWidth / 1.2, window.innerHeight / 1.2);
+  document.body.appendChild(renderer.domElement);
+  function animate() {
+    requestAnimationFrame(animate);
+
+    tickMoveByKey();
+    tickWallBlock();
+    tickSnowBallCollision();
+
+    const frameCount = renderer.info.render.frame;
+    if (frameCount % generateSnowBallTicks === 0) {
+      tickGenerateSnowBalls();
+    }
+    tickSnowballsAndShadow();
+
+    renderer.render(scene, camera);
+  }
+  animate();
+};
---
+++ frontend/src/openingAnimation.js
@@ -0,0 +1,88 @@
+import { Game } from "./game";
+import * as THREE from "three";
+export const OpeningAnimation = (enemyName) => {
+  socket.on("start", () => {
+    Game(enemyName);
+  });
+  socket.emit("start");
+
+  const ballRadius = 10;
+  const ballCount = 8;
+
+  const ballInitialStats = (() => {
+    // {id/initialPosition/speed/direction/createdAt}
+    const getOutsideRandom = () => {
+      const sign = Math.random > 0.5 ? 1 : -1;
+      return sign * Math.random() * Math.random() * 100 + 150;
+    };
+
+    const getVector2Center = (vector3) => {
+      const centerPosition = new THREE.Vector3().random() * 100;
+      return vector3.sub(centerPosition);
+    };
+
+    const ret = [];
+    for (let i = 0; i < ballCount; i++) {
+      const initialPosition = new THREE.Vector3(
+        getOutsideRandom(),
+        getOutsideRandom(),
+        getOutsideRandom()
+      );
+      ret.push({
+        name: "openingSnowball" + Math.random().toString(),
+        initialPosition,
+        speed: Math.random() + 5,
+        direction: getVector2Center(initialPosition),
+        created: Date.now(),
+      });
+    }
+    return ret;
+  })();
+  window.stats = ballInitialStats;
+
+  const generateBalls = () => {
+    ballInitialStats.forEach((ballStats) => {
+      var geometry = new THREE.SphereGeometry(ballRadius, 32, 32);
+      var material = new THREE.MeshBasicMaterial({ color: 0xffffff });
+      var sphere = new THREE.Mesh(geometry, material);
+      sphere.position.x = ballStats.initialPosition.x;
+      sphere.position.y = ballStats.initialPosition.y;
+      sphere.position.z = ballStats.initialPosition.z;
+      scene.add(sphere);
+      sphere.name = ballStats.name;
+    });
+  };
+  generateBalls();
+  const tickBalls = () => {
+    const balls = scene.children.filter((child) =>
+      child.name.includes("openingSnowball")
+    );
+    balls.forEach((ball) => {
+      const ballStats = scene.getObjectById(ball.id);
+      const deltaSecondsFromCreated = (Date.now() - ballStats.createdAt) / 1000;
+      const dest =
+        ballStats.initialPosition +
+        ballStats.direction
+          .multiplyScalar(deltaSecondsFromCreated)
+          .sub(
+            new THREE.Vector3(
+              0,
+              0,
+              4.9 * deltaSecondsFromCreated * deltaSecondsFromCreated
+            ) / 1000
+          );
+      console.log(ballStats.direction);
+      ball.position.x = dest.x;
+      ball.position.y = dest.y;
+      ball.position.z = dest.z;
+    });
+  };
+  window.tickBalls = tickBalls;
+
+  function animate() {
+    requestAnimationFrame(animate);
+    // tickBalls();
+    renderer.render(scene, camera);
+  }
+  animate();
+};
