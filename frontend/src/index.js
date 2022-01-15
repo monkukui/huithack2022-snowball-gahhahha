2b15e32bd7404f0fbc607e169824a6ab4c861ac6
takapiro99 <bigstitch99@gmail.com>
2022-01-15 16:57:37 +0900

    installed socketio

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,1 +1,11 @@
-console.log("hello world")
+import { io } from "socket.io-client";
+
+const socket = io('localhost:8000', {
+  path: '/ws/socket.io'
+});
+
+socket.on("connect", () => {
+  console.log(socket.id);
+});
+
+window.socket = socket
